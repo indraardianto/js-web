92ee00d49fa04a8747d12480be990faffe466b59
indraardianto <indraardianto5@gmail.com>
2024-12-04 11:38:38 +0700

    added santriApp

---
+++ santriApp/app.js
@@ -0,0 +1,36 @@
+class Mahasantri {
+
+    constructor(storageKey) {
+        this.storageKey = storageKey;
+        this.mahasantri = JSON.parse(localStorage.getItem(this.storageKey)) || [];
+        this.jurusan = [
+            {
+                "id": 1,
+                "jurusan": "PPW"
+            },
+            {
+                "id": 2,
+                "jurusan": "PPM"
+            },
+            {
+                "id": 3,
+                "jurusan": "PSJ"
+            }
+        ]
+    }
+
+    saveToStorage() {
+        localStorage.setItem(this.storageKey, JSON.stringify(this.mahasantri));
+    }
+
+    tampilDataSantri() {
+        return this.mahasantri
+    }
+
+    tambahDataSantri(data) {
+        this.mahasantri.push(data);
+        this.saveToStorage();
+    }
+}
+
+const santri = new Mahasantri("appData")
